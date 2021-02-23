d7e371e7fabde16239e80cf105331b63186172e2
eelopez515 <eelopez15@gmail.com>
2021-02-23 10:38:07 -0500

    Initial commit
    initial commit to begin project and emptied read.me

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Covid-19 Tracker</h1>
+    </div>
+  );
+}
+
+export default App;
