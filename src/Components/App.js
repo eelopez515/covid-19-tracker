8e07883999dca4adfe7daa28d4fd601afc759e84
eelopez515 <eelopez15@gmail.com>
2021-02-25 14:37:49 -0500

    Header and InfoBoxes
    created a header which includes the title and a region selector.
    also created info-boxes which will display certain information.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,70 @@
-import React from 'react'
+import { FormControl, MenuItem, Select } from '@material-ui/core';
+import React, { useState, useEffect } from 'react'
+import InfoBox from './InfoBox'
 import '../Styles/App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide')
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+      .then(response => response.json())
+      .then(data => {
+        const countries = data.map(country => (
+          {
+            name: country.country,
+            value: country.countryInfo.iso2
+          }
+        ))
+        setCountries(countries)
+      })
+    }
+    getCountriesData()
+  }, [])
+
+  const onCountryChange = (e) => {
+    const countryCode = e.target.value
+    setCountry(countryCode)
+  }
+
   return (
-    <div className="App">
-      <h1>Covid-19 Tracker</h1>
-      {/* Header */}
-      {/* Title and Region Selector */}
-      {/* InfoBox */}
-      {/* InfoBox */}
-      {/* InfoBox */}
+    <div className="app">
+      <div className="app__header">
+      <h1>COVID-19 TRACKER</h1>
+      <FormControl className='app__dropdown'>
+        <Select
+        variant='outlined'
+        value={country}
+        onChange={onCountryChange}
+        >
+          <MenuItem value='worldwide'>Worldwide</MenuItem>
+          {
+            countries.map(country => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))
+          }
+        </Select>
+      </FormControl>
+      </div>
+      <div className="app__stats">
+          <InfoBox
+          title='Coronavirus Cases'
+          cases={1000}
+          total={2000}
+          />
+          <InfoBox
+          title='Recovered'
+          cases={1000}
+          total={2000}
+          />
+          <InfoBox
+          title='Deaths'
+          cases={1000}
+          total={2000}
+          />
+      </div>
       {/* Table */}
       {/* Graph */}
       {/* Map */}
